8ce2154480fed60e0730429c53ece850dbe4a6dd
andre.martins.lavva <andre.martins@lavva.pt>
2021-10-19 23:11:41 +0100

    Video

--- src/compoments/organisms/banner/data.js
+++ src/compoments/organisms/banner/data.js
@@ -4,7 +4,7 @@ const data = {
   description: 'Assumenda quo cumque adipisci quisquam nam minus ut expedita. Non rerum distinctio aut et rerum at nobis ullam beatae.',
   buttonLabel: 'Explore',
   scrollLabel: 'SCROLL TO EXPLORE',
-  video: '/videos/banner/video.mp4'
+  video: '/videos/banner/peregrine.mp4'
 }
 
 export default data;
